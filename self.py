24c518f43d7b6bd76a1f7c15bd35b8698ddc29ca
artsyompichinko <artsyompichinko@gmail.com>
2024-03-02 22:33:56 +0300

    2.3.2024

--- self.py
+++ self.py
@@ -3,19 +3,3 @@ print(2+2)
 
 print(2+2)
 print(2+2)
-
-
-print(2+2)
-print(2+2)
-print(2+2)
-print(2+2)
-
-print(2+2)
-print(2+2)
-
-print(2+2)
-print(2+2)
-print(2+2)
-print(2+2)
-print(2+2)
-print(2+2)
